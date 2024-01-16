dc360eb3b092dffb924621e99bdc2d9f83fe535a
ShaneyEtkinUcsc <130529293+ShaneyEtkinUcsc@users.noreply.github.com>
2024-01-15 17:59:29 -0800

    Session 1 prompt for Coding Journal 1

---
+++ Session_1_Prompt.py
@@ -0,0 +1,17 @@
+# session 1 prompt
+# print out your name, pronouns,
+# two sentences abt fav movie and food
+
+# tells computer what to print
+def PrintMyInfo():
+	print("Shaney Etkin.\nPronouns she/her.")
+	print("My favorite movie is Labyrinth, it's got great songs and I think it's just overall really fun!\nMy favorite food is boerewors, an Afrikaans grilled sausage.")
+
+# run print function
+def main():
+	PrintMyInfo()
+
+
+# executes the /main/ program when script is run
+if __name__=="__main__":
+    main()
