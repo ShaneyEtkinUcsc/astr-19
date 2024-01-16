83fff0258069c74c7768a0da2bee2c00a338de08
ShaneyEtkinUcsc <130529293+ShaneyEtkinUcsc@users.noreply.github.com>
2024-01-15 18:24:08 -0800

    Session 2 prompt for Code Journal 1

---
+++ Session_2_Prompt.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# define two floats
+x = 2.5
+y = 7.0
+# sum
+z = x+y
+print("sum of floating point numbers = ",z)
+print("type(z): ",type(z))
+
+# define two ints
+a = 5
+b = 3
+# difference
+c = a-b
+print("difference between two integers = ",c)
+print("type(c): ",type(c))
+
+# product of int and float
+d = x*a
+print("product of float and int = ",d)
+print("type(d): ",type(d))
